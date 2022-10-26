006821c5660e44010aa46cc96dd8537a738bc26a
amantayal29 <91675899+amantayal29@users.noreply.github.com>
2022-10-26 21:55:37 +0530

    Create day2.cpp

---
+++ day2.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+       int m=matrix.size();
+       int n=matrix[0].size();
+        vector<int> ans;
+        
+        int top=0,bottom=m-1;
+        int left=0,right=n-1;
+        int i,j;
+        while(top<=bottom && left<=right)
+        {
+            
+            for(i=left;i<=right && top<=bottom;i++)
+            {
+                ans.push_back(matrix[top][i]);
+            }
+            top++;
+            
+            for(i=top;i<=bottom && left<=right;i++)
+            {
+                ans.push_back(matrix[i][right]);
+            }
+            right--;
+            for(i=right;i>=left && top<=bottom;i--)
+            {
+                ans.push_back(matrix[bottom][i]);
+            }
+            bottom--;
+            for(i=bottom;i>=top && left<=right;i--)
+            {
+                ans.push_back(matrix[i][left]);
+            }
+            left++;
+            
+            
+            
+        }
+         return ans;   
+    }
+};
